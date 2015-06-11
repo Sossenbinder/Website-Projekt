7d3f9f605d7f640a78404fcd39dc57f182d11f65
calvinkehl <samuel.kehl@gmail.com>
2015-06-11 10:22:55 +0200

    made it work ;)

--- PHP/header.php
+++ PHP/header.php
@@ -1,9 +1,9 @@
-<div class="header">
+<div class="topMenu">
   <!-- <div class="companyFontDiv">
     <span class="companyFont">Klaviermanufaktur Lorentz</span>
   </div> -->
   <div class="logoImage">
-    <a href="home.html"><img src="../Resources/Pictures/logo.png"></a>
+    <a href="home.php"><img src="../Resources/Pictures/logo.png"></a>
   </div>
   <a href="http://www.facebook.com" id="facebookTag" class="facebookTag"></a>
   <a href="https://plus.google.com/u/0/112766873149357913973/posts" id="googleplusTag" class="googleplusTag"></a>
@@ -11,9 +11,9 @@
 </div>
 
 <div class="menu">
-  <a href="home.html" class="menuElement"> <span>Home</span>  </a>
-  <a href="produkte.html" class="menuElement menuElementTwo"> <span>Produkte</span> </a>
-  <a href="kunden.html" class="menuElement menuElementThree"> <span>Kunden</span> </a>
-  <a href="kontakte.html" class="menuElement menuElementFour"> <span>Kontakt</span> </a>
-  <a href="impressum.html" class="menuElement menuElementFive"> <span>Impressum</span> </a>
+  <a href="home.php" class="menuElement"> <span>Home</span>  </a>
+  <a href="produkte.php" class="menuElement menuElementTwo"> <span>Produkte</span> </a>
+  <a href="kunden.php" class="menuElement menuElementThree"> <span>Kunden</span> </a>
+  <a href="kontakte.php" class="menuElement menuElementFour"> <span>Kontakt</span> </a>
+  <a href="impressum.php" class="menuElement menuElementFive"> <span>Impressum</span> </a>
 </div>
