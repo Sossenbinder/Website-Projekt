ef2690feec504696abf00aaf0fa68e3545cae6b0
calvinkehl <samuel.kehl@gmail.com>
2015-06-11 09:54:06 +0200

    added php & a bit of structure

---
+++ PHP/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+  <title>Klaviermanufaktur Lorentz</title>
+  <link rel="stylesheet" type="text/css" href="../CSS/general.css">
+</head>
+
+<body>
+  <div class="wrapper">
+    
+    <?php
+      include('header.php');
+    ?>
+
+    <div class="contentParent">
+      <div class="siteContent">
+
+      </div>
+    </div>
+    <div class="footer">
+      <!-- footer -->
+    </div>
+  </div>
+</body>
