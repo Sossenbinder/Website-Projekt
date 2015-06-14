27c2b61ff135f343536ac39912c448eb15177ddc
samu <samuel.kehl@gmail.com>
2015-06-14 19:01:15 +0200

    added content to home

--- PHP/home.php
+++ PHP/home.php
@@ -1,4 +1,55 @@
 
   <div class="siteContent">
-    Das ist der Content
+  	<link rel="stylesheet" type="text/css" href="../CSS/home.css">
+	<!-- Place this tag in your head or just before your close body tag. -->
+	<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+
+  	<div class="welcome">
+  		<h1>Wilkommen bei Piano Lorentz!</h1>
+  		<p>
+  			<img src="../Resources/Pictures/klavierhaus.jpg"> <br /> <br />
+  			Hier finden Pianisten aller Art, was ihr Herz begehrt: eine Auswahl an Klavieren, Fl&uumlgeln und speziellen Tasteninstrumenten zum kleinen Preis! <br />
+  			<br />
+  			Bla bla bla...
+  		</p>
+  	</div>
+
+	<div class="social">  	
+		<div class="socialHead">
+		<h1>Neuigkeiten von unseren Social-Sites:</h1>
+		</div>
+	  	<div class="twitterTimeline">
+		    <a class="twitter-timeline" href="https://twitter.com/PianoLorentz" data-widget-id="610113760607535104">Tweets von @PianoLorentz </a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+		</div>
+		<div class="googlePosts">
+			<div class="googlePost">
+				<!-- Place this tag where you want the widget to render. -->
+				<div class="g-post" data-href="https://plus.google.com/112766873149357913973/posts/Grs4Er76DT1"></div>
+			</div>
+			<div class="googlePost">
+				<!-- Place this tag where you want the widget to render. -->
+				<div class="g-post" data-href="https://plus.google.com/112766873149357913973/posts/Y8om8Nvpd4j"></div>
+			</div>
+		</div>
+		<div class="facebookFeed">
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));</script>
+
+
+			<div class="fb-page" data-href="https://www.facebook.com/PianoLorentz" data-width="500" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true">
+				<div class="fb-xfbml-parse-ignore">
+					<blockquote cite="https://www.facebook.com/PianoLorentz">
+						<a href="https://www.facebook.com/PianoLorentz">Piano Lorentz</a>
+					</blockquote>
+				</div>
+			</div>
+		</div>
+	</div>
   </div>
