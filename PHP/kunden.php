e59306c7144654e4be24df7c43c92580bdb81faa
Sossenbinder <Stefan.Daniel.Schranz@t-online.de>
2015-06-17 15:24:55 +0200

    Some cleanup and changes
    Heute abend mach ich da weiter :)

--- PHP/kunden.php
+++ PHP/kunden.php
@@ -1,4 +1,44 @@
+<div class="siteContent"  id="siteContent">
+    <p>Wir haben es noch keine Sekunde bereut, wir haben sehr viel Spaß mit dem Klavier.</p>
+    <p> </p>
+    <p>Sehr zufrieden! Neutrale Beratung, ohne Zeitdruck.</p>
+    <p> </p>
+    <p>Die Beratung war gut. Die Möglichkeit Probezuspielen war sehr gut. Wir waren sehr</p>
+    <p>zufrieden.</p>
+    <p>(18 Monate nach dem Kauf)</p>
+    <p> </p>
+    <p>Mit unserem Klavier sind wir sehr zufrieden. Wir sind froh, ein höherwertiges Klavier gekauft zu haben. Es gibt kaum einen Tag, an dem auf dem Instrument nicht gespielt wird. Die Beratung war sehr kompetent.</p>
+    <p>(15 Jahre nach dem Kauf)</p>
+    <p> </p>
+    <p>Die ausgezeichnete Beratung ermöglichte uns die richtige Auswahl aus der Angebotspalette zu treffen. Das Preis-Leistungsverhältnis stimmte. Auch nach 14 Jahren sind wir mit dem Klavier nach wie vor hochzufrieden.</p>
+    <p> </p>
+    <p>Die Beratung war umfangreich, wir wurden nicht zum Kauf eines bestimmten Modells</p>
+    <p>gedrängt, hatten viel Zeit zum Überlegen und durften probespielen wie es notwendig war.</p>
+    <p>Bekommen haben wir das Klavier, das dort auf uns gewartet hat.</p>
+    <p> </p>
+    <p>Wir waren mit Beratung sowie Kauf sehr zufrieden!</p>
+    <p> </p>
+    <p>Wir sind 9 Monate nach dem Kauf des gebrauchten Flügels sehr zufrieden mit dem</p>
+    <p>Instrument. Der Flügel wird täglich 5-8 Stunden “bearbeitet” und ist immer noch in gutem</p>
+    <p>Zustand.</p>
+    <p>Beim Kauf sind wir freundlich und kompetent beraten worden. Vielen Dank!</p>
+    <p> </p>
 
-    <div class="siteContent">
-      <p> Hier würden die Kunden stehen </p>
-    </div>
+    <h3><p>senden Sie uns gerne eine
+     <script type='text/javascript'>
+     <!--
+     var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+     var path = 'hr' + 'ef' + '=';
+     var addy33654 = 's&#101;rv&#105;c&#101;' + '&#64;';
+     addy33654 = addy33654 + 'kl&#97;v&#105;&#101;r-w&#97;gn&#101;r' + '&#46;' + 'd&#101;';
+     var addy_text33654 = 'Email';
+     document.write('<a ' + path + '\'' + prefix + ':' + addy33654 + '\'>');
+     document.write(addy_text33654);
+     document.write('<\/a>');
+     </script>
+     <script type='text/javascript'>
+     </script></p>
+    <p> </p>
+    <p>Vielen Dank!</p>
+    </h3>
+</div>
