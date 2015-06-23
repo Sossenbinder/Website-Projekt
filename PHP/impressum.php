dbc68c35771f3bdfeb20b7ad34af8e7f15ec3bf9
jensgerle <jensgerle@gmail.com>
2015-06-23 23:22:31 +0200

    added impressum
    first draft. table not aligned right. feel free to fix it

--- PHP/impressum.php
+++ PHP/impressum.php
@@ -1,6 +1,21 @@
-
-
     <div class="siteContent">
-      <p> Hier würde das Impressum stehen </p>
+	<div class="impressum">
+				<table>
+					<tr> 
+						<td> <img src="../Resources/Pianos/DSC_0226.JPG"> </td>
+						<td> 
+							<p>
+								<h1>Impressum</h1> <br><br>
+								Piano Manufaktur Lorentz <br><br>
+								K&oumlnigsbaupassagen <br><br>
+								K&oumlnigstra&szlige 28 <br><br>
+								71073 Stuttgart <br><br>
+								Kontakt: info@piano-lorentz.de <br><br>
+								0711.2825585 <br><br>
+							</p>
+						</td>
+					</tr>
+				</table>
     </div>
+	</div>
 
