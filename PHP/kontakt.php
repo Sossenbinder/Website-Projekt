3d69a344bb22e8436f4e589e016f9a5ed1c910ce
Dimitris Kotios <dimitris.kotios@gmail.com>
2015-06-23 15:48:30 +0200

    Changes Kontaktformular

--- PHP/kontakt.php
+++ PHP/kontakt.php
@@ -1,105 +1,105 @@
 <div class="siteContent">
 
 <?php
-error_reporting(E_ERROR | E_PARSE);
+  error_reporting(E_ERROR | E_PARSE);
 
-@require('config.php');
+  @require('config.php');
 
-if ($_POST['delete'])
-{
-unset($_POST);
-}
-
-if ($_POST["kf-km"]) {
-  $anrede         = $_POST["anrede"];
-  $vorname        = $_POST["vorname"];
-  $name           = $_POST["name"];
-  $adresse        = $_POST["adresse"];
-  $ortplz         = $_POST["ortplz"];
-  $telefon        = $_POST["telefon"];
-  $email          = $_POST["email"];
-  $betreff        = $_POST["betreff"];
-  $nachricht      = $_POST["nachricht"];
-  $date           = date("d.m.Y | H:i");
-  $ip             = $_SERVER['REMOTE_ADDR'];
-  $UserAgent      = $_SERVER["HTTP_USER_AGENT"];
-  $host           = getHostByAddr($remote);
-
-  $anrede         = stripslashes($anrede);
-  $vorname        = stripslashes($vorname);
-  $name           = stripslashes($name);
-  $email          = stripslashes($email);
-  $betreff        = stripslashes($betreff);
-  $nachricht      = stripslashes($nachricht);
-
-if (isset($anrede) && $anrede == "0") {
-  $fehler['anrede'] = "<font color=#cc3333>Bitte w&auml;hlen Sie eine <strong>Anrede</strong> aus.<br /></font>";
-}
-
-if(!$vorname) {
-  $fehler['vorname'] = "<font color=#cc3333>Geben Sie bitte Ihren <strong>Vornamen</strong> ein.<br /></font>";
-}
-
-if(!$name) {
-  $fehler['name'] = "<font color=#cc3333>Geben Sie bitte Ihren <strong>Nachnamen</strong> ein.<br /></font>";
-}
-
-if(!$adresse) {
-  $fehler['adresse'] = "<font color=#cc3333>Geben Sie bitte Ihre <strong>Adresse</strong> ein.<br /></font>";
-}
-
-if(!$ortplz) {
-  $fehler['ortplz'] = "<font color=#cc3333>Geben Sie bitte den <strong>PLZ</strong> und den <strong>Ort</strong> ein.<br /></font>";
-}
-
-if (!preg_match("/^[0-9a-zA-ZÄÜÖ_.-]+@[0-9a-z.-]+\.[a-z]{2,6}$/", $email)) {
-   $fehler['email'] = "<font color=#cc3333>Geben Sie bitte Ihre <strong>E-Mail-Adresse</strong> ein.\n<br /></font>";
-}
-
-if(!$betreff) {
- $fehler['betreff'] = '<font color=#cc3333>Bitte waehlen Sie einen <strong>Betreff</strong>.<br /></font>';
-}
-
-if(!$nachricht) {
- $fehler['nachricht'] = '<font color=#cc3333>Geben Sie bitte eine <strong>Nachricht</strong> ein.<br /></font>';
-}
-
-if(!$telefon){
-  $fehler['telefon'] = '<font color=#cc3333>Geben Sie bitte eine <strong>Telefonnummer</strong> ein.<br /></font>';
-}
-
-  if ($anrede && $vorname && $name && $adresse && $ortplz && $telefon && $email && $betreff && $nachricht){
-
-    $mail1="piano.lo";
-    $mail2="rentz@g";
-    $mail3="mail.com";
-
-    $headers = "From: "." <".$email.">";
-
-    $nachricht .= "\n\n\nBitte melden sie sich bei mir: \n".$adresse." ".$ortplz."\nOder auch telefonisch: ".$telefon;
-    $nachricht .= "\n\nMit freundlichen Grueßen,\n".$anrede." ".$vorname." ".$name;
-
-
-    if(mail($mail1.$mail2.$mail3, $betreff, $nachricht, $headers)){
-      echo "<META HTTP-EQUIV=\"refresh\" content=\"0;URL=".$danke."\">";
-    };
-
-    exit;
+  if ($_POST['delete']) {
+    unset($_POST);
   }
-}
 
+  if ($_POST["kf-km"]) {
+    $anrede         = $_POST["anrede"];
+    $vorname        = $_POST["vorname"];
+    $name           = $_POST["name"];
+    $adresse        = $_POST["adresse"];
+    $ortplz         = $_POST["ortplz"];
+    $telefon        = $_POST["telefon"];
+    $email          = $_POST["email"];
+    $betreff        = $_POST["betreff"];
+    $nachricht      = $_POST["nachricht"];
+    $date           = date("d.m.Y | H:i");
+    $ip             = $_SERVER['REMOTE_ADDR'];
+    $UserAgent      = $_SERVER["HTTP_USER_AGENT"];
+    $host           = getHostByAddr($remote);
+    $anrede         = stripslashes($anrede);
+    $vorname        = stripslashes($vorname);
+    $name           = stripslashes($name);
+    $email          = stripslashes($email);
+    $betreff        = stripslashes($betreff);
+    $nachricht      = stripslashes($nachricht);
+  
+
+    if (isset($anrede) && $anrede == "0") {
+      $fehler['anrede']   = "<font color=#cc3333>Bitte w&auml;hlen Sie eine 
+                      <strong>Anrede</strong> aus.<br /></font>";
+    }
+
+    if(!$vorname) {
+      $fehler['vorname']  = "<font color=#cc3333>Geben Sie bitte Ihren 
+                      <strong>Vornamen</strong> ein.<br /></font>";
+    }
+
+    if(!$name) {
+      $fehler['name']     = "<font color=#cc3333>Geben Sie bitte Ihren 
+                      <strong>Nachnamen</strong> ein.<br /></font>";
+    }
+
+    if(!$adresse) {
+      $fehler['adresse']  = "<font color=#cc3333>Geben Sie bitte Ihre 
+                      <strong>Adresse</strong> ein.<br /></font>";
+    }
+
+    if(!$ortplz) {
+      $fehler['ortplz']   = "<font color=#cc3333>Geben Sie bitte den 
+                      <strong>PLZ</strong> und den <strong>Ort</strong> ein.<br /></font>";
+    }
+
+    if (!preg_match("/^[0-9a-zA-ZÄÜÖ_.-]+@[0-9a-z.-]+\.[a-z]{2,6}$/", $email)) {
+      $fehler['email']    = "<font color=#cc3333>Geben Sie bitte Ihre 
+                      <strong>E-Mail-Adresse</strong> ein.\n<br /></font>";
+    }
+
+    if(!$betreff) {
+     $fehler['betreff']    = "<font color=#cc3333>Bitte waehlen Sie einen 
+                      <strong>Betreff</strong>.<br /></font>";
+    }
+
+    if(!$nachricht) {
+     $fehler['nachricht']  = "<font color=#cc3333>Geben Sie bitte eine 
+                      <strong>Nachricht</strong> ein.<br /></font>";
+    }
+
+    if(!$telefon){
+      $fehler['telefon']   = "<font color=#cc3333>Geben Sie bitte eine 
+      <strong>Telefonnummer</strong> ein.<br /></font>";
+    }
+
+    if ($anrede && $vorname && $name && $adresse && $ortplz && $telefon && $email && $betreff && $nachricht){
+      $mail1="piano.lo";
+      $mail2="rentz@g";
+      $mail3="mail.com";
+
+      $headers = "From: "." <".$email.">";
+
+      $nachricht .= "\n\n\nBitte melden sie sich bei mir: \n".$adresse." ".$ortplz."\nOder auch telefonisch: ".$telefon;
+      $nachricht .= "\n\nMit freundlichen Grueßen,\n".$anrede." ".$vorname." ".$name;
+
+      if(mail($mail1.$mail2.$mail3, $betreff, $nachricht, $headers)){
+        echo "<META HTTP-EQUIV=\"refresh\" content=\"0;URL=".$danke."\">";
+      };
+      exit;
+    }
+  }
 ?>
-
-
-
 
 <div class="kontaktformular" id="ktf">
   <link rel="stylesheet" type="text/css" href="../CSS/general.css">
   <link rel="stylesheet" type="text/css" href="../CSS/kontakte.css">
 
   <form action="<?php $_SERVER['PHP_SELF'];?>" method="post" enctype="multipart/form-data">
-    <p></p>
-
+    
     <fieldset class="kontaktdaten">
       <legend>Kontaktdaten</legend>
       <table>
@@ -107,7 +107,7 @@ if(!$telefon){
           <td class="label"><label>Anrede: <span class="pflichtfeld">*</span></label></td>
           <td class="field">
             <?php if ($fehler["anrede"] != "") { echo $fehler["anrede"]; } ?>
-            <select style="width: 100px;" name="anrede" <?php if ($fehler["anrede"] != "") { echo 'class="errordesignfields"'; } ?>>
+            <select style="width: 70px;" name="anrede" <?php if ($fehler["anrede"] != "") { echo 'class="errordesignfields"'; } ?>>
               <option selected="selected" value="0"></option>
               <option value="Herr" <?php if($_POST['anrede']=="Herr"){ echo "selected";}?>>Herr</option>
               <option value="Frau" <?php if($_POST['anrede']=="Frau"){ echo "selected";}?>>Frau</option>
@@ -180,8 +180,7 @@ if(!$telefon){
         </tr>
       </table>
     </fieldset>
-    <p></p>
-
+    
     <fieldset class="anfrage">
       <legend>Anfrage</legend>
       <table>
@@ -202,11 +201,11 @@ if(!$telefon){
 
       	<tr>
        		<td class="label"><label>Nachricht: <span class="pflichtfeld">*</span></label></td>
-       		<td class="field"><?php if ($fehler["nachricht"] != "") { echo $fehler["nachricht"]; } ?><textarea name="nachricht" style="width:200px;border-right: 1px solid #ddd;border-bottom: 1px solid #ddd;border-left: 1px solid #666666;border-top: 1px solid #666666;padding: 0px 3px;margin: 3px 0;"  cols="30" rows="8" <?php if ($fehler["nachricht"] != "") { echo 'class="errordesignfields"'; } ?>><?php echo $_POST[nachricht]; ?></textarea></td>
+       		<td class="field"><?php if ($fehler["nachricht"] != "") { echo $fehler["nachricht"]; } ?><textarea name="nachricht" cols="30" rows="6" <?php if ($fehler["nachricht"] != "") { echo 'class="errordesignfields"'; } ?>><?php echo $_POST[nachricht]; ?></textarea></td>
       	</tr>
       </table>
     </fieldset>
-    <p> </p>
+  
     <?php
       for ($i=0; $i < $cfg['NUM_ATTACHMENT_FIELDS']; $i++) {
         echo '<fieldset class="upload">';
@@ -218,8 +217,8 @@ if(!$telefon){
 
     <fieldset class="buttons">
       <legend>Ihre Aktion</legend>
-      <br />
-      <div>Hinweis: Felder mit <span class="pflichtfeld"><strong>*</strong></span> m&uuml;ssen ausgef&uuml;llt werden.<br /><br />
+        <div>Hinweis: Felder mit <span class="pflichtfeld"><strong>*</strong></span> m&uuml;ssen ausgef&uuml;llt werden.
+        <br>
         <input type="submit" name="kf-km" value="Senden" />
         <input type="submit" name="delete" value="L&ouml;schen" />
       </div>
