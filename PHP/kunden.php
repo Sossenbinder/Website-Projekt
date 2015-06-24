399ba3f009c070de9457d3444dca26c745851495
arashmor <arashmor@gmail.com>
2015-06-24 14:40:35 +0200

    KundenSeite fertig

--- PHP/kunden.php
+++ PHP/kunden.php
@@ -1,34 +1,35 @@
-<div class="siteContent"  id="siteContent">
-    <div class="customerTextDiv">
-      <h1><u><strong> Kundenmeinungen zu unseren Produkten: </strong></u></h1>
-      <div class="customerTextDiv">
-        <p>Wir haben es noch keine Sekunde bereut, wir haben sehr viel Spaß mit dem Klavier.</p>
-        <br>
-        <p>Sehr zufrieden! Neutrale Beratung, ohne Zeitdruck.</p>
-        <br>
-        <p>Die Beratung war gut. Die Möglichkeit Probezuspielen war sehr gut. Wir waren sehr</p>
-        <p>zufrieden.</p>
-        <p>(18 Monate nach dem Kauf)</p>
-        <br>
-        <p>Mit unserem Klavier sind wir sehr zufrieden. Wir sind froh, ein höherwertiges Klavier gekauft zu haben. Es gibt kaum einen Tag, an dem auf dem Instrument nicht gespielt wird. Die Beratung war sehr kompetent.</p>
-        <p>(15 Jahre nach dem Kauf)</p>
-        <br>
-        <p>Die ausgezeichnete Beratung ermöglichte uns die richtige Auswahl aus der Angebotspalette zu treffen. Das Preis-Leistungsverhältnis stimmte. Auch nach 14 Jahren sind wir mit dem Klavier nach wie vor hochzufrieden.</p>
-        <br>
-        <p>Die Beratung war umfangreich, wir wurden nicht zum Kauf eines bestimmten Modells</p>
-        <p>gedrängt, hatten viel Zeit zum Überlegen und durften probespielen wie es notwendig war.</p>
-        <p>Bekommen haben wir das Klavier, das dort auf uns gewartet hat.</p>
-        <br>
-        <p>Wir waren mit Beratung sowie Kauf sehr zufrieden!</p>
-        <br>
-        <p>Wir sind 9 Monate nach dem Kauf des gebrauchten Flügels sehr zufrieden mit dem</p>
-        <p>Instrument. Der Flügel wird täglich 5-8 Stunden “bearbeitet” und ist immer noch in gutem</p>
-        <p>Zustand.</p>
-        <p>Beim Kauf sind wir freundlich und kompetent beraten worden. Vielen Dank!</p>
-        <br>
-      </div>
+﻿<div class="siteContent" style="background-image:url(../Resources/pictures/background-image.jpg)" id="siteContent">
+    
+      
+     <ul>
+<li> <span class="companyFont">Wir haben es noch keine Sekunde bereut, wir haben sehr viel Spa&szlig; mit dem Klavier.</span><p></li>
 
-      <h3>Gerne k&ouml;nnen sie sich mit uns &uuml;ber <a href="index.php?page=kontakt">E-Mail</a> in Kontakt setzen, um eine Bewertung abzugeben!
-      </h3>
-    </div>
+
+      <span class="companyFont">Sehr zufrieden!Neutrale Beratung,ohne Zeitdruck.</span><p>
+
+ <span class="companyFont">Mit unserem Klavier sind wir sehr zufrieden. Wir sind froh, ein h&ouml;herwertiges Klavier gekauft zu haben. Es gibt kaum einen Tag, an dem auf dem Instrument nicht gespielt wird,Die Beratung war sehr kompetent.</span><p>
+
+ <span class="companyFont">Die ausgezeichnete Beratung erm&ouml;glichte uns die richtige Auswahl aus der Angebotspalette zu treffen. Das Preis-Leistungsverh&auml;ltnis stimmte. Auch nach 14 Jahren sind wir mit dem Klavier nach wie vor hochzufrieden.</span><p>
+
+ <span class="companyFont">Angebot an gebrauchten Klavieren war sehr gut. (Wir haben auf Anhieb eines gefunden)</span><p>
+ 
+ <span class="companyFont">Beratung war gut. Die M&ouml;glichkeit Probezuspielen war sehr gut. Wir waren sehr zufrieden!</span><p>
+
+ <span class="companyFont">Wir freuen uns auch &uuml;ber IHRE R&uuml;ckmeldung zum Kauf bei Klavier Lorentz</span><p>
+ <span class="companyFont"> senden Sie uns gerne eine
+ <script type='text/javascript'>
+ <!--
+ var prefix = '&#109;a' + 'i&#108;' + '&#116;o';
+ var path = 'hr' + 'ef' + '=';
+ var addy33654 = 's&#101;rv&#105;c&#101;' + '&#64;';
+ addy33654 = addy33654 + 'kl&#97;v&#105;&#101;r-w&#97;gn&#101;r' + '&#46;' + 'd&#101;';
+ var addy_text33654 = 'Email';
+ document.write('<a ' + path + '\'' + prefix + ':' + addy33654 + '\'>');
+ document.write(addy_text33654);
+ document.write('<\/a>'); 
+ </script>
+ <script type='text/javascript'> 
+ </script><p></span>
+ <span class="companyFont"> Vielen Dank!</span>
+</span> 
 </div>
