89f32cd7368dfdf160046fa8e6cda96f2d5d8d1c
arashmor <arashmor@gmail.com>
2015-07-07 08:46:06 +0200

    Endlich fertig

--- PHP/kunden.php
+++ PHP/kunden.php
@@ -9,38 +9,42 @@
  </style>
 
   <style>
-  div.dis2 {
+  div.dis2 {    
     
-    
-    color: white
-    
+    color: white;
+       
  }	
  </style>
     
  <div class="dis">   
      
- <span class="companyFont"><div class="dis2"><h5>Klara G: &#8222;Wir haben es noch keine Sekunde bereut, wir haben sehr viel Spa&szlig; mit dem Klavier.&#8221;</h5></span><p></div>
+ <div class="dis2">Klara G: &#8222;Wir haben es noch keine Sekunde bereut, wir haben sehr viel Spa&szlig; mit dem Klavier.&#8221;<p></div>
 
+ <div class="dis2">Bastian K: &#8222;Sehr zufrieden! Neutrale Beratung, ohne Zeitdruck.&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Bastian K: &#8222;Sehr zufrieden! Neutrale Beratung, ohne Zeitdruck.&#8221;</h5></span><p></div>
+ <div class="dis2">Stefan Z: &#8222;Mit unserem Klavier sind wir sehr zufrieden. Wir sind froh, ein h&ouml;herwertiges Klavier gekauft zu haben. Es gibt kaum einen Tag, an dem auf dem Instrument nicht gespielt wird, die Beratung war sehr kompetent.&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Stefan Z: &#8222;Mit unserem Klavier sind wir sehr zufrieden. Wir sind froh, ein h&ouml;herwertiges Klavier gekauft zu haben. Es gibt kaum einen Tag, an dem auf dem Instrument nicht gespielt wird, die Beratung war sehr kompetent.&#8221;</h5></span><p></div>
+ <div class="dis2">Albert T: &#8222;Die ausgezeichnete Beratung erm&ouml;glichte uns die richtige Auswahl aus der Angebotspalette zu treffen. Das Preis-Leistungsverh&auml;ltnis stimmte. Auch nach 14 Jahren sind wir mit dem Klavier nach wie vor hochzufrieden.&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Albert T: &#8222;Die ausgezeichnete Beratung erm&ouml;glichte uns die richtige Auswahl aus der Angebotspalette zu treffen. Das Preis-Leistungsverh&auml;ltnis stimmte. Auch nach 14 Jahren sind wir mit dem Klavier nach wie vor hochzufrieden.&#8221;</h5></span><p></div>
-
- <span class="companyFont"><div class="dis2"><h5>Alex H: &#8222;Angebot an gebrauchten Klavieren war sehr gut. Wir haben auf Anhieb eines gefunden.&#8221;</h5></span><p></div>
+ <div class="dis2">Alex H: &#8222;Angebot an gebrauchten Klavieren war sehr gut. Wir haben auf Anhieb eines gefunden.&#8221;<p></div>
  
- <span class="companyFont"><div class="dis2"><h5>Sara R: &#8222;Die Beratung war gut. Die M&ouml;glichkeit Probezuspielen war sehr gut. Wir waren sehr zufrieden!&#8221;</h5></span><p></div>
+ <div class="dis2">Sara R: &#8222;Die Beratung war gut. Die M&ouml;glichkeit Probezuspielen war sehr gut. Wir waren sehr zufrieden!&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Arnold S: &#8222;Beim Kauf sind wir freundlich und kompetent beraten worden. Vielen Dank!&#8221;</h5></span><p></div>
+ <div class="dis2">Arnold S: &#8222;Beim Kauf sind wir freundlich und kompetent beraten worden. Vielen Dank!&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Bruce L: &#8222;Wir waren mit der Beratung sehr zufrieden, vor allem dass alles so schnell und reibungslos verlaufen ist.&#8221;</h5></span><p></div>
+ <div class="dis2">Bruce L: &#8222;Wir waren mit der Beratung sehr zufrieden, vor allem dass alles so schnell und reibungslos verlaufen ist.&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h5>Brad P: &#8222;Der Antransport und der Stimmservice war perfekt, f&#252;r R&#252;ckfragen war Firma Lorentz stets erreichbar.&#8221;</h5></span><p></div>
+ <div class="dis2">Brad P: &#8222;Der Antransport und der Stimmservice war perfekt, f&#252;r R&#252;ckfragen war Firma Lorentz stets erreichbar.&#8221;<p></div>
 
- <span class="companyFont"><div class="dis2"><h4> "Wir freuen uns auch &uuml;ber IHRE R&uuml;ckmeldung zum Kauf bei Klavier Lorentz"</h4></span><p></div>
+ <div class="dis2">Barbara Q: &#8222;Total zufrieden! Sehr freundliche, auf die individuellen Kundenw&#252;nsche eingehende Beratung.&#8221;<p></div>
 
- <span class="companyFont"> <div class="dis2"><h5>senden Sie uns gerne eine
+ <div class="dis2">Danial A: &#8222;Wir waren sowohl mit Beratung, Kauf als auch dem Transport/Aufbau sehr zufrieden.&#8221;<p></div>
+ 
+ <div class="dis2">Dirk V: &#8222;Sehr zufrieden, es war absolut kompetent, freundlich und hilfsbereit. Uns hat es geholfen und vor allem viel Freude bereitet.&#8221;<p></div>
+
+ <div class="dis2"> "Wir freuen uns auch &uuml;ber IHRE R&uuml;ckmeldung zum Kauf bei Klavier Lorentz"<p></div>
+
+  <div class="dis2">senden Sie uns gerne eine
 
  <script type='text/javascript'>
  <!--
@@ -53,8 +57,7 @@
  document.write('<\/a>'); 
  </script>
  <script type='text/javascript'> 
- </script><p></span>
-<span class="companyFont"><div class="dis2"><h4> Vielen Dank!</h5></span></div></div>  
-</span> 
+ </script><p>
+<div class="dis2"> Vielen Dank!</div></div> 
 </div>
 </div>
